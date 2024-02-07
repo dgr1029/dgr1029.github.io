c4a7c5e77d2742ca6283028659c838f3036e8a93
dgr1029 <158447617+dgr1029@users.noreply.github.com>
2024-02-07 10:44:37 +0100

    script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const oblicz = () => {
       break
       case '÷':
 	  if(aktualne === 0) {
-		 wyczyscWynik()
+		 zaktualizujWynik(Błąd)
 		 return
 	  }
         dzialanie = poprzednie / aktualne
